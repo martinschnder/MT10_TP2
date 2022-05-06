af791ac013ec3ce485b8313ba3c7eb4acb2887be
Martin <martin.schneider2001@gmail.com>
2022-05-06 11:31:26 +0200

    tout cassé

--- sourceCode.py
+++ sourceCode.py
@@ -33,6 +33,92 @@ def dicho_rec_mod(x, n, N):
         if (n % 2 == 0):
             return dicho_rec(x*x, n/2)
         else: return x*dicho_rec(x*x, (n-1)/2)
+
+def chooseSize(l, N):
+    for size in reversed(range(floor(ln(N)/ln(2) + 1))):
+        if l * 8 % size == 0:
+            return size
+
+def numerise_safe(message, N):
+    result = []
+    binaryMessage = BinaryStrings().encoding(message)
+    size = chooseSize(len(message), N)
+    for i in range(0, len(binaryMessage), size):
+        sum = 0
+        for j in range(size):
+            sum += int(str(binaryMessage[i+j])) * pow(2, size - j - 1)
+        result.append(sum)
+    result.append(size)
+    return result
+
+def toBin(n, size):
+    res = bin(n)[2:]
+    if len(res) < size:
+        res = (size-len(res))*'0' + res
+    return res
+
+def alphabetise_safe(message, N) :
+    result = []
+    size = message.pop(-1)
+    for digit in message:
+        result.append(toBin(int(digit), size))
+    binaryMessage = ''.join(result)
+    alphabeticMessage = ''
+    for i in range(0, len(binaryMessage), 8):
+        alphabeticMessage = alphabeticMessage + chr(int(binaryMessage[i:8+i], 2))
+    return(alphabeticMessage)
+
+import random
+def cleRSA (m):
+    result = {}
+    result["p"] = chooseP(m)
+    result["q"] = chooseQ(m)
+    result["e"] = chooseE(result["p"], result["q"])
+    result["N"] = result["p"]*result["q"]
+    result["d"] = inv_modulo(result["e"], (result["p"]-1)*(result["q"]-1))
+    result.pop("p")
+    result.pop("q")
+    return result
+
+def inv_modulo(x, m):
+    (p, u, v) = xgcd(x, m)
+    if p == 1: return u % abs(m)
+    else: raise Exception("%s et %s ne sont pas premiers entre eux" % (x, m))
+
+def chooseP(m):
+    return randPremier(int((1 / sqrt(2)) * pow(2,m/2)), int(pow(2,m/2)))
+
+def chooseQ(m):
+    return randPremier(int(1/sqrt(2)*pow(2,m/2)),int(pow(2,m/2)))
+
+def chooseE(p, q):
+    phi = (p-1)*(q-1)
+    while 1:
+        e = randint(1, phi)
+        if gcd(e, phi) == 1 :
+            return e
+
+def protocole1(message, signature, Na, Nb, Nc = 256, ea = 0, eb = 0, da = 0, db = 0):
+    if 3 * Nc >= Na or 3 * Nc >= Nb:
+        print("Veuillez rentrez une valeur plus petite pour Nc")
+        return 1
+    if da and eb:
+        # chiffrage
+        m1c = numerise_safe(message, 3 * Nc)
+        s1c = numerise_safe(signature, 3 * Nc)
+        m2c = encode_rsa(m1c, eb, Nb)
+        s2c = encode_rsa(s1c, da, Na)
+        print("Message et signature cryptés")
+        return(m2c, s2c)
+    if db and ea:
+        # déchiffrage
+        m1c = decode_rsa(message, db, Nb)
+        s1c = decode_rsa(signature, ea, Na)
+        m1 = alphabetise_safe(m1c, 3 * Nc)
+        s1 = alphabetise_safe(s1c, 3 * Nc)
+        print("Message et signature décryptés")
+        print(m1, s1)
+        return(m1, s1)
         
 def factorisationRSA (N):
     L0 = 1
